b21df3deafdb89c2598c9b070e3e1303c8783ab5
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-09-12 20:16:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  gh repo clone matrixbotz0/PETER-PARKER-BOT
+  git clone https://github.com/TeamKazukai/WEDNESDAY-NEW.git /WEDNESDAY-NEW
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PETER-PARKER-BOT
+  git clone $UPSTREAM_REPO /WEDNESDAY-NEW
 fi
-cd /PETER-PARKER-BOT
+cd /WEDNESDAY-NEW
 pip3 install -U -r requirements.txt
-echo "Starting Bot..."
+echo "Starting Bot...."
 python3 bot.py
